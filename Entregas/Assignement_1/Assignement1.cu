bcaa96c7a7c47b1f8786e8e32e423becec7a2fc2
LuisAriasC <luis.ac945@gmail.com>
2018-09-02 15:12:08 -0500

    Tarea 1 check idx 1.0.31

--- Entregas/Assignement_1/Assignement1.cu
+++ Entregas/Assignement_1/Assignement1.cu
@@ -152,7 +152,7 @@ int main(int argc, char **argv)
     SAFE_CALL(cudaMemcpy(d_MatB, h_B, nBytes, cudaMemcpyHostToDevice), "Error copying d_MatB");
 
     // invoke kernel at host side
-    int dimx = 128;
+    int dimx = nx;
     dim3 block(dimx, 1);
     dim3 grid((nx + block.x - 1) / block.x, ny);
 
