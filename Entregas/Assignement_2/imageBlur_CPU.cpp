d06b9ca9e0cd0b0d8276b9c0d376d34702851849
LuisAriasC <luis.ac945@gmail.com>
2018-09-12 20:12:08 -0500

    Assignment 2.0.0

--- Entregas/Assignement_2/imageBlur_CPU.cpp
+++ Entregas/Assignement_2/imageBlur_CPU.cpp
@@ -15,7 +15,7 @@
 #include <opencv2/highgui/highgui.hpp>
 
 #define default_input_image "image.jpg"
-#define blurM_size 5
+#define blurM_size 15
 
  using namespace std;
 
@@ -152,7 +152,7 @@ void blur_CPU(const cv::Mat& input_Image, cv::Mat& output_Image, int blur_size){
 
 	memcpy(output_Image.ptr(), output, inputBytes * sizeof(unsigned char));
 
-  cv::imwrite("output4.jpg", output_Image);
+  cv::imwrite("output5.jpg", output_Image);
 }
 
 int main(int argc, char *argv[]){
