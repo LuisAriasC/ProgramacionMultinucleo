aff18ab3e86aa49ca1b0a47cebf7270b1828dbea
LuisAriasC <luis.ac945@gmail.com>
2018-10-21 12:32:21 -0500

    Assignment4

--- Entregas/Assignment_4/histogramEqualization.cu
+++ Entregas/Assignment_4/histogramEqualization.cu
@@ -84,9 +84,9 @@ void convert_to_gray(const cv::Mat& input, cv::Mat& output, string imageName){
 
 	// Copy back data from destination device meory to OpenCV output image
 	SAFE_CALL(cudaMemcpy(output.ptr(), d_output, grayBytes, cudaMemcpyDeviceToHost), "CUDA Memcpy Host To Device Failed");
-  
+
   //Write the black & white image
-  cv::imwrite(to_string(img_dest) + "bw_" + to_string(imageName) , output);
+  cv::imwrite(to_string("Images/bw_" + imageName , output);
 
 	// Free the device memory
 	SAFE_CALL(cudaFree(d_input), "CUDA Free Failed");
