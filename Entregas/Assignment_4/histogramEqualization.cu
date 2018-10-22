318625493ea9e56b6e53f00b3c8335e55db2e7d7
LuisAriasC <luis.ac945@gmail.com>
2018-10-21 21:40:55 -0500

    Assignment4

--- Entregas/Assignment_4/histogramEqualization.cu
+++ Entregas/Assignment_4/histogramEqualization.cu
@@ -278,7 +278,7 @@ int main(int argc, char *argv[]){
 
 	//Convert image to gray and equalize
 	histogram_equalization(input, output, eq_output, inputImage);
-
+  printf("Sale");
   /*
 	//Allow the windows to resize
 	namedWindow("Input", cv::WINDOW_NORMAL);
