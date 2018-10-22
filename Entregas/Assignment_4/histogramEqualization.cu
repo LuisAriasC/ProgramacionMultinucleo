0e1e10ce3ac756636f423258b3cb7cc8f3ba80f0
LuisAriasC <luis.ac945@gmail.com>
2018-10-21 19:38:23 -0500

    Assignment4

--- Entregas/Assignment_4/histogramEqualization.cu
+++ Entregas/Assignment_4/histogramEqualization.cu
@@ -195,7 +195,7 @@ void convert_to_gray(const cv::Mat& input, cv::Mat& output, cv::Mat& eq_output, 
 
   equalize(histogram, f_histogram, imSize);
   //SAFE_CALL(cudaMemset(d_histogram, 0, C_SIZE * sizeof(int)), "Error setting d_MatC to 0");
-  SAFE_CALL(cudaMemcpy(df_histogram, f_histogram, C_SIZE * sizeof(int), cudaMemcpyDeviceToHost), "CUDA Memcpy Host To Device Failed");
+  SAFE_CALL(cudaMemcpy(df_histogram, f_histogram, C_SIZE * sizeof(int), cudaMemcpyHostToDevice), "CUDA Memcpy Host To Device Failed");
 
   int sum = 0;
   for (int i = 0; i < C_SIZE; i++)
